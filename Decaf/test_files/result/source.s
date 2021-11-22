d04047dbc8c88ed88be8c5a629d83d49197a719f
Pepe Martinez <pepemart1996@gmail.com>
2021-11-22 11:43:27 -0600

    quicksort funciona

--- Decaf/test_files/result/source.s
+++ Decaf/test_files/result/source.s
@@ -7,34 +7,34 @@ block0:
 	STR	 X14, [sp, #24]
 	LDR	X2, [sp, #24]
 	MOV	X2, X2
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X3, #8
-	STR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
 	MUL	X4, X2, X3
-	STR	X4, [sp, #112]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X4, [sp, #120]
 	ADD	X4, X4, #0
 	LDR	X14, [X13, X4]
-	STR	X14, [sp, #120]
-	LDR	X5, [sp, #120]
+	STR	X14, [sp, #128]
+	LDR	X5, [sp, #128]
 	MOV	X5, X5
 	STR	X5, [sp, #32]
 	LDR	X6, [sp, #24]
 	MOV	X2, X6
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #104]
 	MOV	X7, X2
 	STR	X7, [sp, #40]
 block1:
 	LDR	X6, [sp, #24]
 	MOV	X3, X6
-	STR	X3, [sp, #104]
+	STR	X3, [sp, #112]
 	MOV	X2, #2
-	STR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
 	CMP	X3, X2
 	B.LE	l0
 	MOV	X3, #0
@@ -42,30 +42,30 @@ block1:
 l0:
 	MOV	X3, #1
 l1:
-	STR	X3, [sp, #104]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X3, [sp, #112]
 	MOV	X1, #0
 	CMP	X1, X3
 	B.EQ	block5
 block2:
 	LDR	X6, [sp, #24]
 	MOV	X2, X6
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X4, #8
-	STR	X4, [sp, #112]
-	LDR	X2, [sp, #96]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X2, [sp, #104]
+	LDR	X4, [sp, #120]
 	MUL	X8, X2, X4
-	STR	X8, [sp, #120]
-	LDR	X8, [sp, #120]
+	STR	X8, [sp, #128]
+	LDR	X8, [sp, #128]
 	ADD	X8, X8, #0
 	LDR	X14, [X13, X8]
-	STR	X14, [sp, #128]
+	STR	X14, [sp, #136]
 	LDR	X5, [sp, #32]
 	MOV	X2, X5
-	STR	X2, [sp, #96]
-	LDR	X9, [sp, #128]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X9, [sp, #136]
+	LDR	X2, [sp, #104]
 	CMP	X9, X2
 	B.LT	l2
 	MOV	X3, #0
@@ -73,53 +73,53 @@ block2:
 l2:
 	MOV	X3, #1
 l3:
-	STR	X3, [sp, #104]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X3, [sp, #112]
 	MOV	X1, #0
 	CMP	X1, X3
 	B.EQ	block4
 block3:
 	LDR	X6, [sp, #24]
 	MOV	X2, X6
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X4, #8
-	STR	X4, [sp, #112]
-	LDR	X2, [sp, #96]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X2, [sp, #104]
+	LDR	X4, [sp, #120]
 	MUL	X8, X2, X4
-	STR	X8, [sp, #120]
-	LDR	X8, [sp, #120]
+	STR	X8, [sp, #128]
+	LDR	X8, [sp, #128]
 	ADD	X8, X8, #0
 	LDR	X14, [X13, X8]
-	STR	X14, [sp, #128]
-	LDR	X9, [sp, #128]
+	STR	X14, [sp, #136]
+	LDR	X9, [sp, #136]
 	MOV	X5, X9
 	STR	X5, [sp, #32]
 	LDR	X6, [sp, #24]
 	MOV	X2, X6
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #104]
 	MOV	X7, X2
 	STR	X7, [sp, #40]
 block4:
 	LDR	X6, [sp, #24]
 	MOV	X3, X6
-	STR	X3, [sp, #104]
+	STR	X3, [sp, #112]
 	MOV	X2, #1
-	STR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
 	ADD	X3, X3, X2
-	STR	X3, [sp, #104]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X3, [sp, #112]
 	MOV	X6, X3
 	STR	X6, [sp, #24]
 	B	block1
 block5:
 	LDR	X7, [sp, #40]
 	MOV	X2, X7
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #104]
 	MOV	 X15, X2
 	LDR	 X30, [sp, #240]
 	ADD	 sp, sp, #256
@@ -132,18 +132,18 @@ block7:
 	SUB	 sp, sp, 256
 	STR	 X30, [sp, 240]
 	MOV	X3, #0
-	STR	X3, [sp, #104]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X3, [sp, #112]
 	MOV	X10, X3
 	STR	X10, [sp, #48]
 block8:
 	LDR	X10, [sp, #48]
 	MOV	X2, X10
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X3, #2
-	STR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
 	CMP	X2, X3
 	B.LE	l4
 	MOV	X2, #0
@@ -151,89 +151,95 @@ block8:
 l4:
 	MOV	X2, #1
 l5:
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #104]
 	MOV	X1, #0
 	CMP	X1, X2
 	B.EQ	block10
 block9:
 	LDR	X10, [sp, #48]
 	MOV	X3, X10
-	STR	X3, [sp, #104]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X3, [sp, #112]
 	STR	X3, [sp, #24]
 	BL block0
 	MOV	X2, X15
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #104]
 	MOV	X3, X2
 	STR	X3, [sp, #72]
 	LDR	X3, [sp, #72]
 	MOV	X10, X3
-	STR	X10, [sp, #104]
+	STR	X10, [sp, #112]
 	MOV	X4, #8
-	STR	X4, [sp, #112]
-	LDR	X3, [sp, #104]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X3, [sp, #112]
+	LDR	X4, [sp, #120]
 	MUL	X8, X3, X4
-	STR	X8, [sp, #120]
-	LDR	X8, [sp, #120]
+	STR	X8, [sp, #128]
+	LDR	X8, [sp, #128]
 	ADD	X8, X8, #0
 	LDR	X14, [X13, X8]
-	STR	X14, [sp, #128]
-	LDR	X9, [sp, #128]
+	STR	X14, [sp, #136]
+	LDR	X9, [sp, #136]
 	MOV	X5, X9
 	STR	X5, [sp, #64]
 	LDR	X5, [sp, #48]
 	MOV	X2, X5
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X3, #8
-	STR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
 	MUL	X4, X2, X3
-	STR	X4, [sp, #112]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X4, [sp, #120]
 	ADD	X4, X4, #0
 	LDR	X14, [X13, X4]
-	STR	X14, [sp, #120]
-	LDR	X7, [sp, #72]
+	STR	X14, [sp, #128]
+	LDR	X8, [sp, #128]
+	MOV	X7, X8
+	STR	X7, [sp, #80]
+	LDR	X7, [sp, #80]
 	MOV	X2, X7
-	STR	X2, [sp, #96]
-	MOV	X3, #8
-	STR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
-	MUL	X4, X2, X3
-	STR	X4, [sp, #112]
-	LDR	X4, [sp, #112]
-	ADD	X4, X4, #0
-	STR	X8, [X13, X4]
+	STR	X2, [sp, #104]
+	LDR	X7, [sp, #72]
+	MOV	X3, X7
+	STR	X3, [sp, #112]
+	MOV	X4, #8
+	STR	X4, [sp, #120]
+	LDR	X3, [sp, #112]
+	LDR	X4, [sp, #120]
+	MUL	X8, X3, X4
+	STR	X8, [sp, #128]
+	LDR	X8, [sp, #128]
+	ADD	X8, X8, #0
+	STR	X2, [X13, X8]
 	LDR	X7, [sp, #64]
 	MOV	X9, X7
-	STR	X9, [sp, #128]
+	STR	X9, [sp, #136]
 	LDR	X5, [sp, #48]
 	MOV	X2, X5
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X3, #8
-	STR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
 	MUL	X4, X2, X3
-	STR	X4, [sp, #112]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X4, [sp, #120]
 	ADD	X4, X4, #0
 	STR	X7, [X13, X4]
 	LDR	X5, [sp, #48]
 	MOV	X8, X5
-	STR	X8, [sp, #120]
+	STR	X8, [sp, #128]
 	MOV	X2, #1
-	STR	X2, [sp, #96]
-	LDR	X5, [sp, #120]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X5, [sp, #128]
+	LDR	X2, [sp, #104]
 	ADD	X3, X5, X2
-	STR	X3, [sp, #104]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X3, [sp, #112]
 	MOV	X7, X3
 	STR	X7, [sp, #48]
 	B	block8
@@ -244,8 +250,8 @@ block10:
 block11:
 	SUB	 sp, sp, 256
 	STR	 X30, [sp, 240]
-	LDR	 X14, [sp, #336]
-	STR	 X14, [sp, #80]
+	LDR	 X14, [sp, #344]
+	STR	 X14, [sp, #88]
 	LDR	 X30, [sp, #240]
 	ADD	 sp, sp, #256
 	RET
@@ -253,8 +259,8 @@ block12:
 	SUB	 sp, sp, 256
 	STR	 X30, [sp, 240]
 	MOV	X2, #0
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #104]
 	MOV	 X15, X2
 	LDR	 X30, [sp, #240]
 	ADD	 sp, sp, #256
@@ -267,66 +273,66 @@ _start:
 block14:
 	MOV	X13, sp
 	MOV	X3, #0
-	STR	X3, [sp, #104]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X3, [sp, #112]
 	MOV	X7, X3
-	STR	X7, [sp, #88]
+	STR	X7, [sp, #96]
 	MOV	X2, #3
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X3, #0
-	STR	X3, [sp, #104]
+	STR	X3, [sp, #112]
 	MOV	X4, #8
-	STR	X4, [sp, #112]
-	LDR	X3, [sp, #104]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X3, [sp, #112]
+	LDR	X4, [sp, #120]
 	MUL	X5, X3, X4
-	STR	X5, [sp, #120]
-	LDR	X5, [sp, #120]
+	STR	X5, [sp, #128]
+	LDR	X5, [sp, #128]
 	ADD	X5, X5, #0
 	STR	X2, [X13, X5]
 	MOV	X9, #2
-	STR	X9, [sp, #128]
+	STR	X9, [sp, #136]
 	MOV	X2, #1
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X3, #8
-	STR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
 	MUL	X4, X2, X3
-	STR	X4, [sp, #112]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X4, [sp, #120]
 	ADD	X4, X4, #0
 	STR	X9, [X13, X4]
 	MOV	X5, #1
-	STR	X5, [sp, #120]
+	STR	X5, [sp, #128]
 	MOV	X2, #2
-	STR	X2, [sp, #96]
+	STR	X2, [sp, #104]
 	MOV	X3, #8
-	STR	X3, [sp, #104]
-	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
+	STR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
 	MUL	X4, X2, X3
-	STR	X4, [sp, #112]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X4, [sp, #120]
 	ADD	X4, X4, #0
 	STR	X5, [X13, X4]
 	BL block7
 	MOV	X9, X15
-	STR	X9, [sp, #128]
+	STR	X9, [sp, #136]
 	MOV	X7, #0
-	STR	X7, [sp, #136]
-	STR	X2, [sp, #96]
-	LDR	X7, [sp, #136]
+	STR	X7, [sp, #144]
+	STR	X2, [sp, #104]
+	LDR	X7, [sp, #144]
 	MOV	X2, X7
-	STR	X2, [sp, #88]
+	STR	X2, [sp, #96]
 block15:
-	LDR	X2, [sp, #88]
-	MOV	X7, X2
-	STR	X7, [sp, #96]
-	MOV	X3, #2
-	STR	X3, [sp, #104]
 	LDR	X2, [sp, #96]
-	LDR	X3, [sp, #104]
+	MOV	X7, X2
+	STR	X7, [sp, #104]
+	MOV	X3, #2
+	STR	X3, [sp, #112]
+	LDR	X2, [sp, #104]
+	LDR	X3, [sp, #112]
 	CMP	X2, X3
 	B.LE	l6
 	MOV	X2, #0
@@ -334,43 +340,43 @@ block15:
 l6:
 	MOV	X2, #1
 l7:
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #96]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #104]
 	MOV	X1, #0
 	CMP	X1, X2
 	B.EQ	block17
 block16:
-	STR	X2, [sp, #96]
-	LDR	X2, [sp, #88]
+	STR	X2, [sp, #104]
+	LDR	X2, [sp, #96]
 	MOV	X3, X2
-	STR	X3, [sp, #104]
+	STR	X3, [sp, #112]
 	MOV	X4, #8
-	STR	X4, [sp, #112]
-	LDR	X2, [sp, #104]
-	LDR	X4, [sp, #112]
+	STR	X4, [sp, #120]
+	LDR	X2, [sp, #112]
+	LDR	X4, [sp, #120]
 	MUL	X5, X2, X4
-	STR	X5, [sp, #120]
-	LDR	X5, [sp, #120]
+	STR	X5, [sp, #128]
+	LDR	X5, [sp, #128]
 	ADD	X5, X5, #0
 	LDR	X14, [X13, X5]
-	STR	X14, [sp, #128]
-	LDR	X9, [sp, #128]
-	STR	X9, [sp, #80]
+	STR	X14, [sp, #136]
+	LDR	X9, [sp, #136]
+	STR	X9, [sp, #88]
 	BL block11
 	MOV	X7, X15
-	STR	X7, [sp, #96]
-	LDR	X9, [sp, #88]
+	STR	X7, [sp, #104]
+	LDR	X9, [sp, #96]
 	MOV	X2, X9
-	STR	X2, [sp, #104]
+	STR	X2, [sp, #112]
 	MOV	X7, #1
-	STR	X7, [sp, #96]
-	LDR	X2, [sp, #104]
-	LDR	X7, [sp, #96]
+	STR	X7, [sp, #104]
+	LDR	X2, [sp, #112]
+	LDR	X7, [sp, #104]
 	ADD	X2, X2, X7
-	STR	X2, [sp, #104]
-	LDR	X2, [sp, #104]
+	STR	X2, [sp, #112]
+	LDR	X2, [sp, #112]
 	MOV	X9, X2
-	STR	X9, [sp, #88]
+	STR	X9, [sp, #96]
 	B	block15
 block17:
 	mov	X0, #0
